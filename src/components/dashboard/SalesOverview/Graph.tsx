7f63e162551fc9d1ee27a116b9c6aeb7b5b889c3
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2023-01-22 11:49:33 -0300

    feat: adjustment in styles

--- src/components/dashboard/SalesOverview/Graph.tsx
+++ src/components/dashboard/SalesOverview/Graph.tsx
@@ -8,16 +8,16 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
-  CartesianAxis
+  CartesianAxis,
 } from "recharts";
 
 type Data = {
   name: string;
   uv: number;
-  pv: number
-  amt: number
-  mon: string
-}
+  pv: number;
+  amt: number;
+  mon: string;
+};
 
 const data: Data[] = [
   {
@@ -25,96 +25,95 @@ const data: Data[] = [
     uv: 4000,
     pv: 2400,
     amt: 2400,
-    mon: 'Jan'
+    mon: "Jan",
   },
   {
     name: "Page B",
     uv: 3000,
     pv: 1398,
     amt: 2210,
-    mon: 'Fev'
+    mon: "Fev",
   },
   {
     name: "Page C",
     uv: 2000,
     pv: 9800,
     amt: 2290,
-    mon: 'Mar'
+    mon: "Mar",
   },
   {
     name: "Page D",
     uv: 2780,
     pv: 3908,
     amt: 2000,
-    mon: 'Abr'
+    mon: "Abr",
   },
   {
     name: "Page E",
     uv: 1890,
     pv: 4800,
     amt: 2181,
-    mon: 'Mai'
+    mon: "Mai",
   },
   {
     name: "Page F",
     uv: 2390,
     pv: 3800,
     amt: 2500,
-    mon: 'Jun'
+    mon: "Jun",
   },
   {
     name: "Page G",
     uv: 3490,
     pv: 4300,
     amt: 2100,
-    mon: 'Jul'
+    mon: "Jul",
   },
   {
     name: "Page H",
     uv: 3490,
     pv: 4300,
     amt: 2100,
-    mon: 'Ago'
+    mon: "Ago",
   },
   {
     name: "Page I",
     uv: 3490,
     pv: 4300,
     amt: 2100,
-    mon: 'Set'
+    mon: "Set",
   },
   {
     name: "Page J",
     uv: 3490,
     pv: 4300,
     amt: 2100,
-    mon: 'Out'
+    mon: "Out",
   },
   {
     name: "Page J",
     uv: 3490,
     pv: 4300,
     amt: 2100,
-    mon: 'Nov'
+    mon: "Nov",
   },
   {
     name: "Page K",
     uv: 3490,
     pv: 4300,
     amt: 2100,
-    mon: 'Dez'
+    mon: "Dez",
   },
- 
 ];
 
 export const Graph = () => {
   return (
-     <ResponsiveContainer  width={'100%'} height={300}>
-    <LineChart  data={data}>
-      <CartesianAxis x={10} strokeDasharray={"1"} />
-      <XAxis dataKey={'mon'} />
-      <Line type="natural"  dataKey="pv" stroke="#8884d8" strokeWidth={3} />
-    </LineChart>
+    <ResponsiveContainer width={"100%"} height={300}>
+      <LineChart data={data}>
+        <CartesianAxis x={10} strokeDasharray={"1"} />
+        <XAxis dataKey={"mon"} />
+        <Line type="natural" dataKey="pv" stroke="#8884d8" strokeWidth={3} />
+      </LineChart>
     </ResponsiveContainer>
   );
 };
--- src/components/dashboard/SalesOverview/index.tsx
+++ src/components/dashboard/SalesOverview/index.tsx
@@ -4,7 +4,7 @@ import { Graph } from './Graph'
 
 export const SalesOverview = () => {
   return (
-    <div className='rounded-lg p-4 drop-shadow-md bg-white'>
+    <div className='rounded-lg p-4 shadow-sm bg-white'>
       <h3 className='text-slate-700 text-base font-semibold'>Sales Overview</h3>
       <div className='flex items-center gap-1 mb-6'>
         <ArrowUp className='text-green-500' />
